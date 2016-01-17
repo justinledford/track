b613bbd2de4618bb8f8835e40c728bcbaa4107f9
justin <justinledford@gmail.com>
2016-01-16 23:10:29 -0600

    support for UPS tracking

--- track.py
+++ track.py
@@ -12,6 +12,10 @@ import requests
 import yaml
 
 CONFIG_PATH = "{}/.config/track/config.yaml".format(os.environ['HOME'])
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) \
+                  AppleWebKit/537.36 (KHTML, like Gecko) \
+                  Chrome/34.0.1847.131 Safari/537.36'
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -54,12 +58,9 @@ def fix_usps_datetime(date_time):
 
 # Make request and return OrderedDict of status updates
 def get_usps_data(tracking_number):
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) \
-                  AppleWebKit/537.36 (KHTML, like Gecko) \
-                  Chrome/34.0.1847.131 Safari/537.36'
     url = "https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1="\
             + tracking_number
-    headers = {'user-agent': user_agent}
+    headers = {'user-agent': USER_AGENT} 
     r = requests.get(url, headers=headers)
     
     html = r.text
@@ -111,6 +112,75 @@ def get_usps_data(tracking_number):
     return OrderedDict(sorted(data.items()))
 
 
+def get_ups_data(tracking_number):
+    url = "https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums="\
+            + tracking_number + "&loc=en_us"
+    headers = {'user-agent': USER_AGENT}
+    r = requests.get(url, headers=headers)
+    html = r.text
+
+    soup = BeautifulSoup(html, 'html5lib')
+
+    #
+    # This parses the tracking information for the page
+    # of a package that has already been delivered.
+    # Tracking information is collapsed and expanding
+    # sends an XHR to retrieve it.
+    #
+
+    # Progress is loaded through an XHR, get form parameters here
+    form = soup.select("#detailFormid")[0]
+
+    # Not valid if no form
+    if not form:
+        return None
+
+    hidden = form.select("input")
+    params = {}
+    for e in hidden:
+        params[e['name']] = e['value']
+
+    post_url = "https://wwwapps.ups.com/WebTracking/detail"
+    r = requests.post(post_url, data=params)
+    soup = BeautifulSoup(r.text, 'html5lib')
+
+    table = soup.select(".dataTable")[0]
+    table_rows = table.select("tr + tr")
+
+    data = {}
+
+    # Lots of extra space characters leading, trailing and
+    # in between words
+    for tr in table_rows:
+        location = tr.select("td:nth-of-type(1)")[0]
+        location = remove_whitespace(location.string)
+        location = " ".join(location.split())
+
+        date = tr.select("td:nth-of-type(2)")[0]
+        date = remove_whitespace(date.string).strip()
+
+        time = tr.select("td:nth-of-type(3)")[0]
+        # AM/PM need no periods for strptime
+        time = remove_whitespace(time.string).strip().replace('.','')
+
+        status = tr.select("td:nth-of-type(4)")[0]
+        status = remove_whitespace(status.string)
+        status = " ".join(status.split())
+
+        # Get timestamp for sorting
+        date_time = "{} {}".format(date, time)
+        timestamp = datetime.strptime(date_time,
+                "%m/%d/%Y %I:%M %p").timestamp()
+
+        data[timestamp] = {}
+        data[timestamp]["date_time"] = date_time
+        data[timestamp]["status"] = status
+        data[timestamp]["location"] = location
+
+
+    return OrderedDict(sorted(data.items()))
+
+
 def print_history(history):
     for a, b in history.items():
         print(b['date_time'])
@@ -130,14 +200,18 @@ def list_packages():
 def track_package(package_name):
     config = yaml.load(open(CONFIG_PATH, 'r'), Loader=yaml.BaseLoader)
 
-    for carrier, shipments in config.items():
+    for c, shipments in config.items():
         for number, name in shipments.items():
             if package_name == name:
                 tracking_number = number
+                carrier = c
                 break
 
     try:
-        history = get_usps_data(tracking_number)
+        if carrier == 'USPS':
+            history = get_usps_data(tracking_number)
+        if carrier == 'UPS':
+            history = get_ups_data(tracking_number)
         print_history(history)
     except:
         print("Tracking information not found")
